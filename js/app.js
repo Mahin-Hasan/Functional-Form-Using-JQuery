b909d9b874c171374be7dc742ed8050ecc66dd81
Mahin Hasan <mahinmtrs@gmail.com>
2023-02-27 10:45:12 +0600

    fixed all unique id issue and set value dynamically when plus button is clicked again

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
     // plus button
     // let plusButtonState = false;
-    let initialId = 0;
-
+    // let initialId = 0;
+    // let setInputField = '#input_field' + initialId;
+    // console.log(initialId);
     $('#plus_btn').on('click', function () {
         // if (plusButtonState === false) {
         $('.add-items').append(`  
@@ -19,14 +20,28 @@ $(document).ready(function () {
             <input class="input-style" type="text" placeholder="">
         </div>`);
         //Making dynamic id
-        $('input').each(function () {
-            initialId = initialId + 1;
-            $(this).attr("id", "input_field" + initialId);
-        })
+        function generateId() {
+            let initialId = 0;
+            console.log('executed')
+            $('input').each(function () {
+                initialId = initialId + 1;
+                $(this).attr("id", "input_field" + initialId);
+            })
+        }
+        generateId()
         // }
         // plusButtonState = true;
 
     });
+    // function generateId() {
+    //     let initialId = 0;
+    //     console.log('executed')
+    //     $('input').each(function () {
+    //         initialId = initialId + 1;
+    //         $(this).attr("id", "input_field" + initialId);
+    //     })
+    // }
+    // generateId()
     // close button
     $('#close_btn').on('click', function () {
         console.log('working')
@@ -38,7 +53,6 @@ $(document).ready(function () {
         //     const details = event.target.value;
         //     console.log(details);
         // }) rough code
-
         var input1 = $('#input_field1').val();
         var input2 = $('#input_field2').val();
         var input3 = $('#input_field3').val();
@@ -47,15 +61,15 @@ $(document).ready(function () {
         var input6 = $('#input_field6').val();
         var input7 = $('#input_field7').val();
         var input8 = $('#input_field8').val();
-        // var input9 = $('#input_field9').val();
-        // var input10 = $('#input_field10').val();
-        // var input11 = $('#input_field11').val();
-        // var input12 = $('#input_field12').val();
-        // var input13 = $('#input_field13').val();
-        // console.log(input1, input2, input3);
+        var input9 = $('#input_field9').val();
+        var input10 = $('#input_field10').val();
+        var input11 = $('#input_field11').val();
+        var input12 = $('#input_field12').val();
+        var input13 = $('#input_field13').val();
+        console.log(input1, input2, input3);
         $('#output_field').append(` 
         <div class="mt-3">
-            <h4 class="text-succ ess">Results</h4>
+            <h4 class="text-success">Results</h4>
             <p>Buyers Name: ${input1}</p>
             <p>Mobile: ${input2}</p>
             <p>Address: ${input3}</p>
@@ -65,23 +79,88 @@ $(document).ready(function () {
             <p>Rate: ${input7}</p>
             <p>Amount: ${input8}</p>
         </div>`);
+        if (input9 !== '') {
+            $('#output_field').append(`
+            <div class="mt-3">
+            <p>Product Name: ${input9}</p>
+        </div>`)}
+        if (input10 !== '') {
+            $('#output_field').append(`
+            <div class="mt-3">
+            <p>Description: ${input10}</p>
+        </div>`)}
+        if (input11 !== '') {
+            $('#output_field').append(`
+            <div class="mt-3">
+            <p>Quantity: ${input11}</p>
+        </div>`)}
+        if (input12 !== '') {
+            $('#output_field').append(`
+            <div class="mt-3">
+            <p>Rate: ${input12}</p>
+        </div>`)}
+        if (input13 !== '') {
+            $('#output_field').append(`
+            <div class="mt-3">
+            <p>Amount: ${input13}</p>
+        </div>`)}
+
+
+
         //For showing all input field items in console
         $("input").each(function () {
             var input = $(this).val();
             console.log(input);
         });
-        const total = $('input-style').length;
+        //getting total input field items
+        const total = $('.input-style').length;
         console.log(total);
         $('input').val('');//clearing input field after submit button 
 
 
     });
     //trying dynamic setting value
+
     // $('#submit_btn').on('click', function () {
-    //     $('input').each(function(){
-    //         let details = $(this).val();
-    //         console.log(details);
-    //     } )
+    //     $('input').each(function () {
+    //         let elmId = $(this).attr("id");
+    //         let elementIdFull = '#' + elmId;
+    //         let elementIdFullDot = '"'+ elementIdFull +'"'
+    //         var valueOfInput = $(elementIdFullDot).val();
+    //         console.log(elmId, elementIdFull, elementIdFullDot,valueOfInput);
+
+
+
+
+    //         $('#output_field').append(` 
+    //     <div class="mt-3">
+    //         <h4 class="text-success">Results</h4>
+    //         <p>Buyers Name: ${elmId}</p>
+    //     </div>`);
+
+    //         // set value
+    //     })
+
+
+    //     // $('input').each(function(){
+    //     //     let details = $(this).val();
+    //     //     console.log(details);
+    //     // } )
     // });
 
 });
+
+
+
+  //      $('#output_field').append(`
+        // <div class="mt-3">
+        //     <h4 class="text-success">Results</h4>
+        //     <p>Buyers Name: ${$(elmId).val()}</p>
+        //     <p>Mobile: ${$('#'+elmId).val()}</p>
+        //     <p>Address: ${$(elmId).val()}</p>
+        //     <p>Product Name: ${$(elmId).val()}</p>
+        //     <p>Description: ${$(elmId).val()}</p>
+        //     <p>Quantity: ${$(elmId).val()}</p>
+        //     <p>Rate: ${$(elmId).val()}</p>
+        //     <p>Amount: ${$(elmId).val()}</p>
+        // </div>`);
